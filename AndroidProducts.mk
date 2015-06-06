f071ac0d564000b834ea5997bbb0221b2da0cddc
Dees Troy <dees_troy@teamw.in>
2015-06-06 14:24:28 +0000

    Updates

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,3 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device_a3ul.mk \
-    $(LOCAL_DIR)/du_a3ul.mk
+    $(LOCAL_DIR)/omni_a3ul.mk
--- omni_a3ul.mk
+++ omni_a3ul.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/du/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := a3ul
 
 # Omni Common
-$(call inherit-product, vendor/du/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/a3ul/device_a3ul.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a3ul
-PRODUCT_NAME := du_a3ul
+PRODUCT_NAME := omni_a3ul
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := Desire 610
 PRODUCT_MANUFACTURER := htc
